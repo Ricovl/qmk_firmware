cf26197cfb1da9b5b757d27fdad06a4ad33c54d2
Ricovl <rico@vanleeuwen.dev>
2021-07-03 14:30:15 +0200

    Add personal keymap

---
+++ keyboards/kyria/keymaps/ricovl/config.h
@@ -0,0 +1,49 @@
+/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// #define MASTER_RIGHT
+// #define SPLIT_USB_DETECT
+// #define EE_HANDS
+// #define SPLIT_USB_DETECT
+// #define SPLIT_USB_TIMEOUT 5000
+// #define MASTER_LEFT
+
+#ifdef OLED_DRIVER_ENABLE
+  #define OLED_DISPLAY_128X64
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+  #define RGBLIGHT_SPLIT
+  // #define RGBLIGHT_ANIMATIONS
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_LIMIT_VAL 150
+#endif
+
+#define ENCODER_RESOLUTION 2
+
+#define WS2812_PWM_DRIVER PWMD1  // default: PWMD1
+#define WS2812_PWM_CHANNEL 2  // default: 2
+#define WS2812_PWM_PAL_MODE 6  // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 2
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM5  // DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
+#define WS2812_DMA_CHANNEL 5  // DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
+
+// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
+// #define SPLIT_USB_DETECT
+// #define NO_USB_STARTUP_CHECK
---
+++ keyboards/kyria/keymaps/ricovl/rules.mk
@@ -0,0 +1,16 @@
+CTPC=yes
+
+AUDIO_ENABLE = no
+
+OLED_DRIVER_ENABLE = yes # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes # Enable keyboard RGB underglow
+
+WS2812_DRIVER = pwm
+# SERIAL_DRIVER = usart
+# WS2812_DRIVER = bitbang
+# ifeq ($(strip $(CTPC)), yes)
+# 	# SERIAL_DRIVER = usart
+# endif
+
+# OPT_DEFS += -DSTM32_DMA_REQUIRED=TRUE
